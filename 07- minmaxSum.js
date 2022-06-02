8b7334f211fefcab4dda399085a3ad70fc999cc9
Erika Skarda <erikaskarda@yahoo.com.br>
2022-06-01 21:35:38 -0300

    feat: minMaxSum

---
+++ 07- minmaxSum.js
@@ -0,0 +1,30 @@
+// // function miniMaxSum(arr) {
+//   // Write your code here
+//   let sumMax = 0;
+//   let sumMin = 0;
+
+//   const max = arr.reduce(function(prev, current) {
+//     return(prev > current ? prev: current)
+//   })
+
+//   const min = arr.reduce(function(prev, current) {
+//     return (prev < current ? prev : current)
+//   })
+
+//   for (let i = 0; i < arr.length; i++) {
+//     sumMax += arr[i];
+//     sumMin += arr[i]
+//   }
+
+//   sumMax = sumMax - min;
+//   sumMin = sumMin - max;
+
+//   console.log(sumMax, sumMin)
+// }
+
+function miniMaxSum(arr) {
+  let sumTotal = arr.reduce((a, b) => {
+    return a + b
+  })
+}
+miniMaxSum([1, 3, 5, 7, 9])
